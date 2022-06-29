b3e222846bd47af510a36a988e8a6190f1f78a91
Omer Husain <husain.omer@gmail.com>
2022-06-29 16:42:17 -0400

    created CampaginList and CampaignCard components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.scss";
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import { Container, Row, Col } from "react-bootstrap";
+import CampaignList from "./components/CampaignList/CampaignList";
 
 function App() {
+
+  [campaigns, setCampaigns] = useState(null);
+
+
+  
+
   return (
     <>
       <Header />
       <Container>
         <h1>App.js</h1>
+        <CampaignList />
       </Container>
     </>
   );
---
+++ src/components/CampaignList/CampaignList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CampaignCard from "../CampaignCard/CampaignCard";
+
+const CampaignList = () => {
+  return (
+    <>
+      
+      <CampaignCard></CampaignCard>
+    </>
+  );
+};
+
+export default CampaignList;
