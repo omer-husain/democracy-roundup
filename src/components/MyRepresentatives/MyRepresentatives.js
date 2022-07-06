274526925634353f2817d25108236eae1613a810
Omer Husain <husain.omer@gmail.com>
2022-07-06 10:28:13 -0400

    final commit

--- src/components/MyRepresentatives/MyRepresentatives.js
+++ src/components/MyRepresentatives/MyRepresentatives.js
@@ -75,7 +75,7 @@ const MyRepresentatives = () => {
   ) : (
     <>
       <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
+        <span className="visually-hidden">Loading....</span>
       </Spinner>
     </>
   );
